864057c00b366c89aee5238025fa1671cf928b56
kwanjung <kwanjungkim@naver.com>
2024-07-01 16:50:46 +0900

    remove .git files

---
+++ loain/src/main/java/SchDev/loain/LoainApplication.java
@@ -0,0 +1,13 @@
+package SchDev.loain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoainApplication.class, args);
+	}
+
+}
---
+++ loain/src/test/java/SchDev/loain/LoainApplicationTests.java
@@ -0,0 +1,13 @@
+package SchDev.loain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
